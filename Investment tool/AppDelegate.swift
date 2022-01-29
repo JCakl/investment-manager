f78e280257fbae902ac23b7900bc6ea241f02d6c
JCakl <jan.cakl@yahoo.com>
2022-01-29 16:43:41 +0100

    load csv files, header processing (broker identification)

--- Investment tool/AppDelegate.swift
+++ Investment tool/AppDelegate.swift
@@ -10,7 +10,20 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    enum BrokerName: String, CaseIterable  {
+        case trading212 = "Trading212"
+        case revolut = "Revolut"
+        case degiro = "Degiro"
+        case unknown = "Unknown"
+    }
     
+    struct Broker {
+        var name = BrokerName.unknown.rawValue
+    }
+    
+    class File{
+        var broker = Broker()
+    }
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
@@ -24,7 +37,81 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-
+    
+    func checkHeader( header : String ) -> BrokerName.RawValue {
+        
+        // TODO: set currency via UI
+        let currency = "CZK"
+        
+        let headerTrading212 = ["Action", "Time", "ISIN", "Ticker", "Name", "No. of shares", "Price / share" , "Currency (Price / share)", "Exchange rate", "Result (\(currency))", "Total (\(currency))", "Withholding tax", "Currency (Withholding tax)", "Charge amount (\(currency))", "Deposit fee (\(currency))", "Notes", "ID", "Currency conversion fee (\(currency))"]
+        
+        let columns = header.split(separator: ",", omittingEmptySubsequences: false)
+        
+        for broker in BrokerName.allCases {
+            for column in columns {
+                if !headerTrading212.contains(String(column)) {
+                    break
+                }
+            }
+            return broker.rawValue
+        }
+        
+        return BrokerName.unknown.rawValue
+    }
+    
+    func parseCSVfile( csvFile : String ) -> Bool {
+        let lines = csvFile.split(separator: "\n")
+        
+        var broker = Broker()
+        broker.name = checkHeader( header : String(lines[0]) )
+        
+        print("Broker name is <\(broker.name)>")
+        
+        for line in lines.dropFirst() {
+            let columns = line.split(separator: ",", omittingEmptySubsequences: false)
+            
+            // TODO: process columnsß
+            /*for column in columns {
+                
+            }*/
+            
+        }
+        return true
+    }
+    
+    func processCsvFile( panel: NSOpenPanel ) -> Bool {
+        var csvData = ""
+        for file in panel.urls{
+            do{
+                csvData = try String(contentsOf: file, encoding: .utf8)
+                if( !parseCSVfile( csvFile: csvData ) ){
+                    return false
+                }
+            }catch{
+                return false
+            }
+        }
+        return true
+    }
+    
+    @IBAction func importCSV(_ sender: Any) {
+        
+        let openPanel = NSOpenPanel()
+        openPanel.canChooseFiles = true
+        openPanel.allowsMultipleSelection = true
+        openPanel.allowedFileTypes = ["csv"]
+        
+        if (openPanel.runModal() == NSApplication.ModalResponse.OK) {
+            if( processCsvFile( panel : openPanel ) ){
+                print("CSV file(s) are imported correctly")
+            }else{
+                print("CSV file(s) are not imported correctly")
+            }
+        }
+        
+        
+    }
+    
 
 }
 
